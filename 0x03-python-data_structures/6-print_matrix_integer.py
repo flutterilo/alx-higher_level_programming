8faf3dcf120b31f9cb8ac6db695552556a297a3a
Ilyass <ilias.kassimi@gmail.com>
2023-06-12 17:40:17 +0000

    6-print_matrix_integer

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i in row:
+            if i == row[len(row) - 1]:
+                print(i)
+                continue;
+            print(i, end = ' ')
