ea6c2e16d280ad94725a988a8804d2e319eaecae
Ilyass <ilias.kassimi@gmail.com>
2023-06-12 17:58:11 +0000

    6-print_matrix_integer

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if matrix == [[]]:
+        print("")
+        return
     if isinstance(matrix, list):
         for row in matrix:
             if isinstance(row, list):
