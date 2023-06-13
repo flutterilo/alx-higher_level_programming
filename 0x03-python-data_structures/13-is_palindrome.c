80ca60886027dc923797902ed6e1099f86f1e0cb
Ilyass <ilias.kassimi@gmail.com>
2023-06-13 15:59:14 +0000

    13-is_palindrom.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,68 +1,55 @@
 #include "lists.h"
 
-
 /**
- * reverse_listint - Reverses a singly-linked listint_t list.
- * @head: A pointer to the starting node of the list to reverse.
- * Return: A pointer to the head of the reversed list.
+ * is_palindrome - check if a list is palindrom or not
+ * @head: address head of list
+ * Return: 1 if palindrom or 0 if not
  */
-listint_t *reverse_listint(listint_t **head)
-{
-	listint_t *node = *head, *next, *prev = NULL;
 
-	while (node)
-	{
-		next = node->next;
-		node->next = prev;
-		prev = node;
-		node = next;
-	}
-
-	*head = prev;
-	return (*head);
-}
-
-/**
- * is_palindrome - Checks if a singly linked list is a palindrome.
- * @head: A pointer to the head of the linked list.
- *
- * Return: If the linked list is not a palindrome - 0.
- */
 int is_palindrome(listint_t **head)
 {
-	listint_t *tmp, *rev, *mid;
-	size_t size = 0, i;
+	listint_t *tmp;
+	listint_t *rev_list;
+	listint_t *tmp_rev;
 
-	if (*head == NULL || (*head)->next == NULL)
-		return (1);
-
+	tmp = *head;
+	rev_list = reverse_list(tmp);
+	tmp_rev = rev_list;
 	tmp = *head;
 	while (tmp)
 	{
-		size++;
-		tmp = tmp->next;
-	}
-
-	tmp = *head;
-	for (i = 0; i < (size / 2) - 1; i++)
-		tmp = tmp->next;
-
-	if ((size % 2) == 0 && tmp->n != tmp->next->n)
-		return (0);
-
-	tmp = tmp->next->next;
-	rev = reverse_listint(&tmp);
-	mid = rev;
-
-	tmp = *head;
-	while (rev)
-	{
-		if (tmp->n != rev->n)
+		if (tmp->n != rev_list->n)
+		{
+			free_listint(tmp_rev);
 			return (0);
+		}
 		tmp = tmp->next;
-		rev = rev->next;
+		rev_list = rev_list->next;
 	}
-	reverse_listint(&mid);
-
+	free_listint(tmp_rev);
 	return (1);
+}
+
+/**
+* reverse_list - reverse list
+* @list: list base to be reversed
+* Return: return new list reversed
+*/
+
+listint_t *reverse_list(listint_t *list)
+{
+	listint_t *new_list;
+	listint_t *tmp;
+	listint_t *prev = NULL;
+
+	tmp = list;
+	while (tmp)
+	{
+		new_list = malloc(sizeof(listint_t));
+		new_list->next = prev;
+		new_list->n = tmp->n;
+		prev = new_list;
+		tmp = tmp->next;
+	}
+	return (new_list);
 }
