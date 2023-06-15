ad4b834f6a58e4148e224705c59693130c6e467c
Ilyass <ilias.kassimi@gmail.com>
2023-06-15 01:35:25 +0000

    10-best_score.py

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+        return None
+
+    key = list(a_dictionary.keys())[0]
+    big = a_dictionary[key]
+    for k, v in a_dictionary.items():
+        if v > big:
+            big = v
+            key = k
+    return (key)
