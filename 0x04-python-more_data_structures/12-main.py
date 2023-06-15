7e2cd3470317b4e26a76ce2c88f592d34a4f04a3
Ilyass <ilias.kassimi@gmail.com>
2023-06-15 16:43:32 +0000

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-main.py
+++ 0x04-python-more_data_structures/12-main.py
@@ -17,4 +17,6 @@ print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
 roman_number = "DCCVII"
 print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+roman_number = "MDCCLXXVI"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,17 +1,46 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if (not isinstance(roman_string, str) or
-            roman_string is None):
-        return (0)
-    roman = {"I": 1, "V": 5, "IX": 9, "X": 10,
-             "L": 50, "C": 100, "D": 500, "M": 1000}
-    integer = 0
-    for i in range(len(roman_string)):
-        if roman.get(roman_string[i], 0) == 0:
-            return (0)
-        if (i != (len(roman_string) - 1) and
-                roman[roman_string[i]] < roman[roman_string[i + 1]]):
-            integer += roman[roman_string[i]] * -1
-        else:
-            integer += roman[roman_string[i]]
-    return (integer)
+    num = 0
+    i = 0
+    while i < len(roman_string):
+        if roman_string[i] == 'M':
+            num += 1000
+        elif roman_string[i] == 'C':
+            if roman_string[i + 1] == 'M':
+                num += 900
+                i += 1
+            elif roman_string[i + 1] == 'D':
+                num += 400
+                i += 1
+            else:
+                num += 100
+        elif roman_string[i] == 'D':
+            num += 500
+        elif roman_string[i] == 'X':
+            if i + 1 == len(roman_string):
+                num += 10
+            elif roman_string[i + 1] == 'C':
+                num += 90
+                i += 1
+            elif roman_string[i + 1] == 'L':
+                num += 40
+                i += 1
+            else:
+                num += 10
+        elif roman_string[i] == 'L':
+            num += 50
+        elif roman_string[i] == 'I':
+            if i + 1 == len(roman_string):
+                num += 1
+            elif roman_string[i + 1] == 'X':
+                num += 9
+                i += 1
+            elif roman_string[i + 1] == 'V':
+                num += 4
+                i += 1
+            else:
+                num += 1
+        elif roman_string[i] == 'V':
+            num += 5
+        i += 1
+    return num
