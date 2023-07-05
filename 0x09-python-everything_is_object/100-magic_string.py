b15e5ba5c41b78021a4beae2607067ca1fb83e57
Ilyass <ilias.kassimi@gmail.com>
2023-07-05 18:23:08 +0000

    100-margic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,15 +1,4 @@
 #!/usr/bin/python3
-
-
-x = 0
-
-
-def magic_string():
-    '''
-    magic function that return string
-    '''
-    global x
-    x += 1
-    str = "BestSchool, " * (x - 1)
-    str += "BestSchool"
-    return str
+def magic_string(s=[]):
+    s += ["Holberton"]
+    return ", ".join(s)
