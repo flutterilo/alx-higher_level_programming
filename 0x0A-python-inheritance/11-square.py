368e00156ccf36deaef00113340452466f2f0dfd
Ilyass <ilias.kassimi@gmail.com>
2023-07-10 23:52:49 +0000

    11-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This module extends the Rectangle class
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Defines Square class"""
+    def __init__(self, size):
+        """Initializes attribute"""
+        super().integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
+
+    def area(self):
+        """Returns area of the square"""
+        return self.__size**2
