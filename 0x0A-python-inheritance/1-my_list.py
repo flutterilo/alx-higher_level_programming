3a0d904a97ee7e7825eee3af2985ffd41aea5627
Ilyass <ilias.kassimi@gmail.com>
2023-07-10 20:21:18 +0000

    1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+'''
+module call rectangle
+'''
+
+
+class MyList(list):
+    '''
+    return all attricute and function in object
+    '''
+    def print_sorted(self):
+        print(sorted(self))
