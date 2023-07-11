28548c8c30e23e5b2b06a63992dbace0e5436588
Ilyass <ilias.kassimi@gmail.com>
2023-07-11 21:27:47 +0000

    6-load_from_json_file.py

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+module call 6-load_from_json_file
+'''
+import json
+
+
+def load_from_json_file(filename):
+    '''
+    load json from file
+    args:
+        filename: name of file
+    '''
+    with open(filename, "r", encoding="UTF-8") as f:
+        json.load(f)
