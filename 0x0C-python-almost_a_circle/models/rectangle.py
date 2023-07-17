e0320c2b64087de4deb0410f012a547996c8c94b
Ilyass <ilias.kassimi@gmail.com>
2023-07-17 15:32:04 +0000

    validation of all set methodes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -16,3 +16,50 @@ class Rectangle(Base):
         self.x = x
         self.y = y
         super().__init__(id)
+
+    def validator(self, name, value, attr_type):
+        """Validates passed arguments"""
+        if type(value) is not int:
+            raise TypeError(name + ' must be an integer')
+        if attr_type == 1:
+            if value <= 0:
+                raise ValueError(name + ' must be > 0')
+        if attr_type == 2:
+            if value < 0:
+                raise ValueError(name + ' must be >= 0')
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.validator("width", value, 1)
+        self.__width = value
+
+    @property
+    def height(self) -> 'height':
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.validator("height", value, 1)
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.validator("x", value, 2)
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.validator("y", value, 2)
+        self.__y = value
