33799f2e831c14eba095311bc31c28bda2771a78
Ilyass <ilias.kassimi@gmail.com>
2023-09-18 00:36:24 +0000

    3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-'''script that lists all states with a name starting with N'''
+'''
+script that takes in arguments
+and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument
+'''
 
 import MySQLdb
 import sys
@@ -8,9 +12,10 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', user=sys.argv[1],
                          passwd=sys.argv[2], db= sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = '{}'\
-    ORDER BY id ASC".format(sys.argv[4]))
+    cur.execute("SELECT * FROM states \
+    ORDER BY id ASC")
     states = cur.fetchall()
     if states:
         for state in states:
-            print(state)
+            if state[1] == sys.argv[4]:
+                print(state)
