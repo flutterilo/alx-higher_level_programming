96a0dbafe2789ba547ef35425fa4c9e349d1eb1f
Ilyass <ilias.kassimi@gmail.com>
2023-09-30 00:39:09 +0000

    3-error_code.py

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    Handle HTTPError exception from the urlopen request.
+"""
+from urllib import request, error
+from sys import argv
+
+
+if __name__ == "__main__":
+    try:
+        with request.urlopen(argv[1]) as response:
+            print(response.read().decode("utf-8"))
+    except error.HTTPError as e:
+        print(f"Error code: {e.code}")
